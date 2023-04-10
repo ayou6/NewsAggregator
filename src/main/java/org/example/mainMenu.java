299065b71f106eb8845a506489abac6755c64d6a
ayou6 <ayoxb.abbas@gmail.com>
2023-04-10 12:13:51 +0400

    spngboot

---
+++ src/main/java/org/example/mainMenu.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class mainMenu {
+            System.out.println("Welcome to Aggregator!!");
+            System.out.println("Select an option !");
+            System.out.print("/1. Search articles by keyword ");
+            System.out.print("2. Browse articles by category ");
+            System.out.print("3. Display popular articles ");
+            System.out.print("4. Exit ");
+
+
+
+}
