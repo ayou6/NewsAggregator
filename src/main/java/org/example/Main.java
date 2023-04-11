22921cc16232cec6646972833eb99334d30d3281
ayou6 <ayoxb.abbas@gmail.com>
2023-04-11 10:45:40 +0400

    completed the main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static MainMenu menu = new MainMenu();
+    static Scanner scn = new Scanner(System.in);
+
     public static void main(String[] args) {
+        while (true) {
+            menu.showMenu();
+            System.out.println("Select an option :");
+            try {
+                int select = scn.nextInt();
+                scn.nextLine();
 
+                switch (select) {
+                    case 1:
+                        break;
 
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        System.out.print("Good Bye !!!");
+                        System.exit(0);
+                        break;
+
+                    default:
+                        System.out.println("Invalid input, please try again.\n");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input, please try again.\n");
+                scn.nextLine();
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/MainMenu.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MainMenu {
+    public void showMenu() {
+                System.out.println("Welcome to Aggregator!!");
+                System.out.println("Select an option !");
+                System.out.println("1. Search articles by keyword ");
+                System.out.println("2. Browse articles by category ");
+                System.out.println("3. Display popular articles ");
+                System.out.println("4. Exit ");
+            }
+
+
+}
